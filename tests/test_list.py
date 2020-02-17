61da58f6486c125356f55f32b7378110a2e550d0
Blair Conrad <blair@blairconrad.com>
2020-02-17 08:47:29 -0500

    Sort appointments as soon as we get them from Outlook

--- tests/test_list.py
+++ tests/test_list.py
@@ -45,6 +45,27 @@ def test_next_appointment_shows_next_only(config, mocker, capsys):
     assert expected_output == captured.out
 
 
+def test_next_appointment_shows_next_only_even_when_results_not_sorted(config, mocker, capsys):
+    expected_output = """
+2020-02-20 12:00:00 [Subject] [Location]
+""".lstrip()
+
+    found_appointments = [
+        Appointment(start="2020-02-20 13:30"),
+        Appointment(start="2020-02-20 12:00"),
+        Appointment(start="2020-02-20 13:45"),
+    ]
+    mocker.patch("where_to.outlook.find_appointments_between", return_value=found_appointments)
+
+    config.which = "next"
+    application = Application(config)
+
+    application.run()
+
+    captured = capsys.readouterr()
+    assert expected_output == captured.out
+
+
 def test_next_appointment_shows_multiple_with_same_start(config, mocker, capsys):
     expected_output = """
 2020-02-20 08:30:00 [Subject] [Location]
